816aefb1572608606c434140d5a1c61dcb5e87e6
sheisburrito <chloejoe5@gmail.com>
2024-03-13 22:08:04 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="container-fluid text-center"> <!-- FOOTER -->
+    <div class="container"> 
+<div class="row">
+
+<div class="col-md-6"> <!-- FOOTER LEFT -->
+<p><a class="navbar-brand" href="#"><span class="josh">JOSH </span> <span class="orange">WHITKIN</span></a></p>
+</div>
+
+<div class="col-md-6"> <!-- FOOTER RIGHT -->
+    <p> TERMS AND CONDITIONS</p> <!-- EDIT TO LINK -->
+    </div>
+
+</div> <!-- row -->
+    </div> <!-- container -->
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>

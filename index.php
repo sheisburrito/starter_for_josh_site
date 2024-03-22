48233e651dcb06744d1d3b9ca79d1c4b4e6a3877
sheisburrito <chloejoe5@gmail.com>
2024-03-23 01:47:05 +0800

    Update index.php
    As per week 3 hw

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+          <section class="container-fluid aboutbg text-center">
+            <div class="container">
+                <h2> GAMES &amp; LEVEL DESIGNER </h2>
+                <p class = "particle">  and particle wizard</p>
+                <p class = "about-text">  My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+            <div class="blocker"></div>
+            </div><!-- container-->
+        </section>
+
+<section class="container-fluid ">
+            <div class="container">
+                    <div>
+                        <h3>LATEST ARTICLES</h3>
+                    </div>
+
+                <div class="row"><!--  a row that gives us access to the BS columns-->
+                    <div class="col-md-4 text-center">
+                        <img class="img-fluid" alt=“portait” src="http://170.187.231.66/~mesh12/wp-content/themes/starter_for_josh_site/images/LatestArticle01.jpg">
+                        <h4 class="article-title">A PORTRAIT</h4>
+                        <p class="date" >12 December 2020</p>
+                        <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                        </p>
+                        <a class="readmore" href="#" >CONTINUE READING</a>
+                    </div>
+
+                    <div class="col-md-4 text-center">
+                        <img class="img-fluid" alt=“model” src="http://170.187.231.66/~mesh12/wp-content/themes/starter_for_josh_site/images/LatestArticle02.jpg">
+                        <h4 class="article-title">A MODEL</h4>
+                        <p class="date">12 December 2020</p>
+                        <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                        </p>
+                        <a class="readmore" href="#" >CONTINUE READING</a>
+                    </div>
+
+                    <div class="col-md-4 text-center">
+                        <img class="img-fluid" alt=“game” src="http://170.187.231.66/~mesh12/wp-content/themes/starter_for_josh_site/images/LatestArticle03.jpg">
+                        <h4 class="article-title">A GAMES DESIGN</h4>
+                        <p class="date">12 December 2020</p>
+                        <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                        </p>
+                        <a class="readmore" href="#" >CONTINUE READING</a>
+    </div>
+                    </div><!-- row-->
+                </div><!-- container-->
+               </section><!-- container-fluid-->
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->

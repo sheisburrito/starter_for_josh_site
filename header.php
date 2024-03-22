cb30ec8c524afb49e3c11acb0f8b23dfb51f6e73
sheisburrito <chloejoe5@gmail.com>
2024-03-23 02:32:25 +0800

    Small Updates

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com"> 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
-
 <title> <?php
               if (function_exists('is_tag') && is_tag()) {
                  single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -31,9 +30,7 @@
 <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
 </head>
 <body>
-
-    <header class="container-fluid bg1">
-      <div class="container">
+    <header class="bg1 container-fluid">
         <nav class="navbar navbar-expand-md navbar-dark">
             <!-- Brand -->
             <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
@@ -42,7 +39,6 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-      
         <!-- Navbar links -->
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
           <ul class="navbar-nav ml-auto">
@@ -66,7 +62,5 @@
         <h1 > Games Design For the Future </h1><!--  the main tag line-->
               </div>
     </div><!-- container-->
-
-<!-- very important that this code is added-->
 <?php wp_head(); ?>   
 </header>

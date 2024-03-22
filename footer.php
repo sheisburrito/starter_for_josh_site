9865bfa9def2fa550e741df2bd6d5b830e5b3661
sheisburrito <chloejoe5@gmail.com>
2024-03-23 01:44:54 +0800

    Update footer.php
    As per week 3 hw

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
+
+                <div class="container"><!-- div to hold the content in the middle of the page-->
+                    <div class="row"><!-- a row that gives us access to the BS columns-->
+                        <div class="col-md-6 text-center">
+                          <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+                        </div>
+                        <div class="col-md-6 ">
+                            <p>Terms and Conditions</p>
+                            </div>
+              </div><!-- row-->
+                    </div><!-- container -->
+            </footer><!-- container-fluid-->
+            <?php wp_footer(); ?>
+</body>
+</html>

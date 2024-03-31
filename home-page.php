024d34578a3495d737731d35bb759c3e0b7aebaa
sheisburrito <chloejoe5@gmail.com>
2024-04-01 01:52:21 +0800

    Introduced homepage.php

--- home-page.php
+++ home-page.php
@@ -6,9 +6,10 @@
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
           <section class="container-fluid aboutbg text-center">
             <div class="container">
-                <h2> GAMES &amp; LEVEL DESIGNER </h2>
                 <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
-            </div><!-- container-->
+                <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+              </div><!-- container-->
             <div class="blocker"></div>
         </section>
 
